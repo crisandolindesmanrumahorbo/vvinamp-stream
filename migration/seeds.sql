d04fb8ade6f1a47d230803ca35af832823c6b79c
crisandolindesmanrumahorbo <crisandolin007@gmail.com>
2025-07-29 19:30:47 +0700

    add migration and seed tracks

---
+++ migration/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO tracks (title, duration) VALUES ('memori-baik', '4:47');
+INSERT INTO tracks (title, duration) VALUES ('feel-good-inc', '3:59');
+
+INSERT INTO tracks (title, duration) VALUES ('Bad Bunny, Jhay Cortez - Dakiti (Letra⧸Lyrics)', '3:37');
